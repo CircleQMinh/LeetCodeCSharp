ce948b4041d57d308865ec8fddb6e09933965e21
unknown <quocminh.vutran3105@gmail.com>
2025-01-12 17:55:35 +0700

    Save my solutions.

---
+++ 6. Zigzag Conversion/ZigzagConversion/ZigzagConversion/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ZigzagConversion;
+
+Console.WriteLine("Hello, World!");
+var a = "PAYPALISHIRING";
+var row = 3;
+var b = Solution.Convert(a,row);
+Console.WriteLine(b);
---
+++ 6. Zigzag Conversion/ZigzagConversion/ZigzagConversion/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZigzagConversion
+{
+    public static class Solution
+    {
+        public static string Convert(string s, int numRows)
+        {
+            var result = "";
+            var leng = s.Length;
+            var row = 0;
+            var cycle = 2 * (numRows - 1);
+            if (numRows == 1 || s.Length <= numRows) {
+                return s;
+            }
+            while (row < numRows) {
+
+                var index = row;
+                var period = 2 * row; // increase between 2 number in same row
+                while (index < leng) { 
+                    result += s[index];
+                    if (period != cycle)
+                    {
+                        period = cycle - period; // alternate between two period, 1st and last only have 1 period
+                    }
+                    index += period;
+                }
+                row++;
+            }
+
+            return result;
+        }
+
+    }
+}
