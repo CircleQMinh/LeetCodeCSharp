f701c411baea3a102216507a7ce736b5c6491a24
unknown <quocminh.vutran3105@gmail.com>
2025-01-15 23:09:10 +0700

    Save my solutions.

---
+++ 14. Longest Common Prefix/14. Longest Common Prefix/14. Longest Common Prefix/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14._Longest_Common_Prefix
+{
+    public class Solution
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            var cur = "";
+            var leng = 999;
+            var shortest = "";
+            foreach (var item in strs)
+            {
+                if (item.Length < leng)
+                {
+                    leng = item.Length;
+                    shortest = item;
+                }
+            }
+
+            for (int i = 0; i < shortest.Length; i++)
+            {
+                foreach (var item in strs)
+                {
+                    if (!item[i].Equals(shortest[i]))
+                    {
+                        return cur;
+                    }
+                }
+                cur += shortest[i];
+            }
+            return cur;
+        }
+    }
+}
