3967ac4eb12ab127a2776093207df049c175540e
unknown <quocminh.vutran3105@gmail.com>
2025-03-09 17:44:25 +0700

    Save my solutions.

---
+++ Solution/103. Binary Tree Zigzag Level Order Traversal/Solution.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhVu
+{
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     public int val;
+     *     public TreeNode left;
+     *     public TreeNode right;
+     *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    public class Solution
+    {
+        public IList<IList<int>> ZigzagLevelOrder(TreeNode root)
+        {
+            var result = new List<IList<int>>();
+            if (root == null)
+            {
+
+                return result;
+            }
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            var reverse = true;
+            while (queue.Count > 0)
+            {
+                reverse = !reverse;
+                var count = queue.Count;
+                var list = new List<int>();
+                for (int i = 0; i < count; i++)
+                {
+                    var node = queue.Dequeue();
+                    if (node.left != null)
+                    {
+                        queue.Enqueue(node.left);
+                    }
+                    if (node.right != null)
+                    {
+                        queue.Enqueue(node.right);
+                    }
+                    list.Add(node.val);
+                }
+                if (reverse)
+                {
+                    list.Reverse();
+                }
+                result.Add(list);
+            }
+            return result;
+        }
+    }
+}
