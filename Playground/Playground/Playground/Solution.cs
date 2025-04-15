c7baeb6e39580a3c367696600222f3a0c2b6abbe
unknown <quocminh.vutran3105@gmail.com>
2025-04-15 22:51:14 +0700

    Save my solutions.

--- Playground/Playground/Playground/Solution.cs
+++ Playground/Playground/Playground/Solution.cs
@@ -5745,16 +5745,23 @@ namespace Playground
             var n = grid[0].Length;
             var directions = new (int X, int Y)[] { (-1, 0), (1, 0), (0, -1), (0, 1) };
             var queue = new Queue<(int X, int Y)>();
-
+            var numberOfFresh = 0;
             for (int i = 0; i < m; i++) {
                 for (int j = 0; j < n; j++) {
                     if (grid[i][j] == 2)
                     {
                         queue.Enqueue((i, j));
                     }
+                    if (grid[i][j] == 1)
+                    {
+                        numberOfFresh++;
+                    }
                 }
             }
-
+            if (numberOfFresh == 0)
+            {
+                return 0;
+            }
             while (queue.Count > 0)
             {
                 daysPass++;
---
+++ Solution/345. Reverse Vowels of a String/Solution.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhVu
+{
+    public class Solution
+    {
+        public string ReverseVowels(string s)
+        {
+            var l = 0;
+            var r = s.Length - 1;
+            var arr = s.ToCharArray();
+            var vowels = new HashSet<char>()
+            {
+                'a','i','e','o','u',
+                'A','I','E','O','U',
+            };
+            while (l < r)
+            {
+                if (vowels.Contains(arr[l]) && vowels.Contains(arr[r]))
+                {
+                    var t = arr[l];
+                    arr[l] = arr[r];
+                    arr[r] = t;
+                    l++;
+                    r--;
+                }
+                if (!vowels.Contains(arr[l]))
+                {
+                    l++;
+                }
+                if (!vowels.Contains(arr[r]))
+                {
+                    r--;
+                }
+
+            }
+
+            return new string(arr);
+        }
+    }
+}
