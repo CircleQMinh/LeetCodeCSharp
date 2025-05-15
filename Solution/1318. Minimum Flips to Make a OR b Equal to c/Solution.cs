e7ba85d368ee5666dbc63fea072a48f10cc088ce
unknown <quocminh.vutran3105@gmail.com>
2025-05-15 12:17:24 +0700

    Save my solutions.

--- Solution/1318. Minimum Flips to Make a OR b Equal to c/Solution.cs
+++ Solution/1318. Minimum Flips to Make a OR b Equal to c/Solution.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Solution.1318. Minimum Flips to Make a OR b Equal to c
+namespace MinhVu
 {
     public class Solution
     {
-        
+        public int MinFlips(int a, int b, int c)
+        {
+            var count = 0;
+            int totalBits = sizeof(int) * 8;
+            for (int i = 0; i < totalBits; i++)
+            {
+                int bitA = (a >> i) & 1;
+                int bitB = (b >> i) & 1;
+                int bitC = (c >> i) & 1;
+                if (bitC == 1)
+                {
+                    var inc = bitA == 1 || bitB == 1 ? 0 : 1;
+                    count += inc;
+                }
+                else
+                {
+                    var inc = bitA == 1 && bitB == 1 ? 2 : bitA == 1 || bitB == 1 ? 1 : 0;
+                    count += inc;
+                }
+            }
+            return count;
+        }
     }
 }
